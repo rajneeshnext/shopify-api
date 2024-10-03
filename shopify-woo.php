6d3c45c35d7646645c3b6592f247c27dee82f839
Rajneesh Kumar Saini <rajnish123saini@gmail.com>
2024-10-03 11:43:05 +0530

    Update shopify-woo.php

--- shopify-woo.php
+++ shopify-woo.php
@@ -63,13 +63,6 @@ function getMetaFields($productID, $token){
 }
 
 $payload = @file_get_contents('php://input');
-echo $payload = '{"admin_graphql_api_id":"gid:\/\/shopify\/Product\/9200697475389","body_html":"<p>1 Paper Bag 20c<\/p>","created_at":"2024-03-30T08:29:30+08:00","handle":"paper-bag-otc","id":9200697475389,"product_type":"","published_at":null,"template_suffix":null,"title":"1 Paper Bag 20c","updated_at":"2024-09-23T14:21:59+08:00","vendor":"ZeGerman","status":"draft","published_scope":"web","tags":"","variants":[{"admin_graphql_api_id":"gid:\/\/shopify\/ProductVariant\/49405923393853","barcode":null,"compare_at_price":null,"created_at":"2024-09-23T14:19:16+08:00","id":49405923393853,"inventory_policy":"deny","position":2,"price":"0.20","product_id":9200697475389,"sku":"rrr1111","taxable":true,"title":"Medium \/ White","updated_at":"2024-09-23T14:19:16+08:00","option1":"Medium","option2":"White","option3":null,"image_id":null,"inventory_item_id":51447987077437,"inventory_quantity":0,"old_inventory_quantity":0},{"admin_graphql_api_id":"gid:\/\/shopify\/ProductVariant\/49405923426621","barcode":null,"compare_at_price":null,"created_at":"2024-09-23T14:19:16+08:00","id":49405923426621,"inventory_policy":"deny","position":3,"price":"0.20","product_id":9200697475389,"sku":"rrrw121","taxable":true,"title":"Medium \/ Red","updated_at":"2024-09-23T14:19:16+08:00","option1":"Medium","option2":"Red","option3":null,"image_id":null,"inventory_item_id":51447987110205,"inventory_quantity":0,"old_inventory_quantity":0},{"admin_graphql_api_id":"gid:\/\/shopify\/ProductVariant\/49405923459389","barcode":null,"compare_at_price":null,"created_at":"2024-09-23T14:19:16+08:00","id":49405923459389,"inventory_policy":"deny","position":4,"price":"0.20","product_id":9200697475389,"sku":"rrr1231","taxable":true,"title":"Small \/ White","updated_at":"2024-09-23T14:19:16+08:00","option1":"Small","option2":"White","option3":null,"image_id":null,"inventory_item_id":51447987142973,"inventory_quantity":0,"old_inventory_quantity":0},{"admin_graphql_api_id":"gid:\/\/shopify\/ProductVariant\/49405923492157","barcode":null,"compare_at_price":null,"created_at":"2024-09-23T14:19:16+08:00","id":49405923492157,"inventory_policy":"deny","position":5,"price":"0.20","product_id":9200697475389,"sku":"rr4321","taxable":true,"title":"Small \/ Red","updated_at":"2024-09-23T14:19:16+08:00","option1":"Small","option2":"Red","option3":null,"image_id":null,"inventory_item_id":51447987175741,"inventory_quantity":0,"old_inventory_quantity":0}],"options":[{"name":"Size","id":12097023934781,"product_id":9200697475389,"position":1,"values":["Medium","Small"]},{"name":"Color","id":12097023967549,"product_id":9200697475389,"position":2,"values":["White","Red"]}],"images":[{"id":45771220713789,"product_id":9200697475389,"position":1,"created_at":"2024-05-18T13:05:43+08:00","updated_at":"2024-05-18T13:05:45+08:00","alt":null,"width":1207,"height":1207,"src":"https:\/\/cdn.shopify.com\/s\/files\/1\/0580\/1449\/9010\/files\/B60FC8F5-E405-444F-8809-255597A5165E.jpg?v=1716008745","variant_ids":[],"admin_graphql_api_id":"gid:\/\/shopify\/ProductImage\/45771220713789"}],"image":{"id":45771220713789,"product_id":9200697475389,"position":1,"created_at":"2024-05-18T13:05:43+08:00","updated_at":"2024-05-18T13:05:45+08:00","alt":null,"width":1207,"height":1207,"src":"https:\/\/cdn.shopify.com\/s\/files\/1\/0580\/1449\/9010\/files\/B60FC8F5-E405-444F-8809-255597A5165E.jpg?v=1716008745","variant_ids":[],"admin_graphql_api_id":"gid:\/\/shopify\/ProductImage\/45771220713789"},"media":[{"id":38277692621117,"product_id":9200697475389,"position":1,"created_at":"2024-05-18T13:05:43+08:00","updated_at":"2024-05-18T13:05:45+08:00","alt":null,"status":"READY","media_content_type":"IMAGE","preview_image":{"width":1207,"height":1207,"src":"https:\/\/cdn.shopify.com\/s\/files\/1\/0580\/1449\/9010\/files\/B60FC8F5-E405-444F-8809-255597A5165E.jpg?v=1716008745","status":"READY"},"variant_ids":[],"admin_graphql_api_id":"gid:\/\/shopify\/MediaImage\/38277692621117"}],"variant_gids":[{"admin_graphql_api_id":"gid:\/\/shopify\/ProductVariant\/49405923393853","updated_at":"2024-09-23T06:19:16.000Z"},{"admin_graphql_api_id":"gid:\/\/shopify\/ProductVariant\/49405923426621","updated_at":"2024-09-23T06:19:16.000Z"},{"admin_graphql_api_id":"gid:\/\/shopify\/ProductVariant\/49405923459389","updated_at":"2024-09-23T06:19:16.000Z"},{"admin_graphql_api_id":"gid:\/\/shopify\/ProductVariant\/49405923492157","updated_at":"2024-09-23T06:19:16.000Z"}],"product_event_type":"update"}';
-$product_event_type = $_GET['product_event_type'];
-$payload = str_replace("'", "", $payload);
-$arr_m = json_decode($payload, TRUE);
-print_r($arr_m);exit();
-pricode_update_product($arr_m);
-exit();
 if($payload!=""){
     $flag=false;
     $product_event_type = $_GET['product_event_type'];
@@ -119,7 +112,7 @@ function pricode_update_product($data_product){
                 }
                 $product->set_attributes( $atts );
                 $product->save(); 
-
+                $product->update_meta_data('shopify', 'Connected');
                 foreach($data_product['variants'] as $variant){ 
                     $data = new stdClass();
                     $variantId = wc_get_product_id_by_sku( $variant['sku'] );
@@ -277,6 +270,7 @@ function pricode_create_product($data_product){
                 }
                 $product->set_attributes( $atts );
                 $product->save();
+                $product->update_meta_data('shopify', 'Connected');
                 if(isset($data_product['product_type']) && $data_product['product_type']!="")
                 wp_set_object_terms($product->get_id(), array($data_product['product_type']), 'product_cat');
                 if(isset($data_product['vendor']) && $data_product['vendor']!="")
